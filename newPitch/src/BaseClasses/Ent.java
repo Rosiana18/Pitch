b8ad45a6c3bb4dadd9184e543a39d30fc6149818
Ryan Bridges <rybridges90@gmail.com>
2014-11-28 20:13:10 -0600

    refactored tags, put some documentation in the google doc entitled 'Ryan's Work'

--- newPitch/src/BaseClasses/Ent.java
+++ newPitch/src/BaseClasses/Ent.java
@@ -7,7 +7,7 @@ public class Ent
 	@Id String id;
     
     
-	//personality tags
+/*	//personality tags
 	@Index int community=0;
 	@Index int personal=0;
 	@Index int causeOriented=0;
@@ -17,7 +17,6 @@ public class Ent
 	@Index int education=0;
 	@Index int technology=0;
 	@Index int code=0;
-	@Index int diy=0;
 	@Index int robotics=0;
 	@Index int maths=0;
 	@Index int structured=0;
@@ -25,7 +24,6 @@ public class Ent
 	@Index int physics=0;
 	@Index int chemistry=0;
 	@Index int sports=0;
-	@Index int art=0;
 	@Index int visual=0;
 	@Index int heady=0;
 	@Index int philosophical=0;
@@ -44,10 +42,46 @@ public class Ent
 	@Index int nightOwl=0;
 	@Index int goals=0;
 	@Index int openEnded=0;
+	@Index int cars=0;
+	@Index int food=0; */
+	
+	
+	// ratings
+	@Index int communityService=0;
+	@Index int social=0;
+	@Index int quirky=0;
+	@Index int trendmaker=0;
+	@Index int fun=0;
+	@Index int thoughtProvoking=0;
+	@Index int important=0;
+	
+	// personality
+	@Index int realist=0;
+	@Index int dreamer=0;
+	@Index int philosophical=0;
+	@Index int serious=0;
+	@Index int depthImportant=0;
+	@Index int breadthImportant=0;
+	@Index int morningPerson=0;
+	@Index int weekendy=0;
+	@Index int nightOwl=0;
+	@Index int goalOriented=0;
+	
+	
+	//values
+	@Index int structure=0;
+	@Index int dynamicEnvironment=0;
+	@Index int selfReliance=0;
+	@Index int teamwork=0;
+	@Index int creativeApproach=0;
+	@Index int reliability=0;
+	@Index int impact=0;
+	@Index int enjoyment=0;
 	
 	//categorization tags
-	@Index int definedProduct=0;
-	@Index int abstractProduct=0;
+	@Index int doSomething=0;
+	@Index int makeSomething=0; // tangible
+	@Index int learnSomething=0;
 	@Index int partnerGroup=0;
 	@Index int smallGroup=0;
 	@Index int medGroup=0;
@@ -61,6 +95,22 @@ public class Ent
 	@Index int medProject=0;
 	@Index int longProject=0;
 	@Index int ongoingProject=0;
+	
+	//what is it ~ what do you do?
+	@Index int science=0;
+	@Index int engineering=0;
+	@Index int writing=0;
+	@Index int craft=0; //i.e. woodworking or metallurgy or whatever
+	@Index int fixing=0; //i.e. fixing things like cars or whatever
+	@Index int visualDesign=0;
+	@Index int conceptDesign=0;
+	@Index int event=0; // is it an event, like a gathering, meetings, whatever
+	@Index int teaching=0; //is it a yoga workshop or a an algebra class?
+	@Index int cause=0; // a project to accomplish something
+	@Index int diy=0;
+	@Index int art=0;
+	@Index int music=0;
+	
     
 	Ent()
 	{
---
+++ newPitch/war/search.jsp
@@ -0,0 +1,12 @@
+
+<html>
+	<title>s\ear/ch</title>
+	<head>
+	<h1> This is Search </h1>
+	</head>
+	
+	<body>
+	<br>
+	Content
+	</body>
+</html>
